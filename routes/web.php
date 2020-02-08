ea66cb2230cb4977d772d0732e35bbc47da0a501
unknown <alexirvin70@gmail.com>
2020-02-08 23:46:38 +0000

    removed base web route and added in employee form post and get

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/employeeForm', function () {
+    return view('employeeForm');
 });
+
+Route::post('/employeeForm', )
