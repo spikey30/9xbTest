24b0037dcedf1394f11878b3f97265c05da3f64c
unknown <alexirvin70@gmail.com>
2020-02-11 11:07:06 +0000

    added in delete function to employee controller also added in checks for limits

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -19,6 +19,7 @@ class EmployeeController extends Controller
     {
         $employees = $request->input('employee');
 
+        $roleExceeded = false;
         foreach ($employees as $id=>$person) {
             $employee = Employee::find($id);
 
@@ -26,22 +27,65 @@ class EmployeeController extends Controller
                 'first_name' => $person['firstname'],
                 'last_name' => $person['lastname'],
                 'email' => $person['email'],
-            ])
-            ->save();
-        }
-
-        if(!empty($request->input('firstname'))) {
-            $employee = new Employee();
-            $employee->fill([
-                'first_name' => $request->input('firstname'),
-                'last_name' => $request->input('lastname'),
-                'email' => $request->input('email'),
-                'job_role' => $request->input('jobrole'),
             ]);
 
+            if($employee->job_role != $person['job_role'])
+            {
+                $count = Employee::where('job_role', $person['job_role'])->count();
+
+                if($count == 4) {
+                    $roleExceeded = true;
+                    break;
+                }
+            }
+            $employee->job_role = $person['job_role'];
             $employee->save();
         }
 
+        if ($roleExceeded) {
+                return redirect('employeeForm')->withErrors([
+                    'role' => 'role count exceeded for job role ',
+                ]);
+        }
+
+        if(!empty($request->input('firstname'))) {
+
+            if (count($employees) == 10) {
+                return redirect('employeeForm')->withErrors([
+                    'employees' => 'employee limit reached',
+                ]);
+            }
+
+            $newEmployee = new Employee();
+            $newEmployee->fill([
+                'first_name' => $request->input('firstname'),
+                'last_name' => $request->input('lastname'),
+                'email' => $request->input('email'),
+            ]);
+
+            $count = Employee::where('job_role', $request->input('job_role'))->count();
+
+            if ($count == 4){
+                return redirect('employeeForm')->withErrors([
+                    'role' => 'role count exceeded for job role '. $request->input('job_role'),
+                ]);
+            } else {
+                $newEmployee->job_role = $request->input('job_role');
+            }
+
+            $newEmployee->save();
+        }
+
         return redirect('employeeForm');
+    }
+
+    public function delete(Employee $employee)
+    {
+        if ($employee) {
+            $employee->delete();
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false]);
+        }
     }
 }
