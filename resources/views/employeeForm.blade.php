71a5deb09e0dbef52b516a48f06c3587cf60a048
unknown <alexirvin70@gmail.com>
2020-02-11 11:10:29 +0000

    added in js to allow for ajax deleting

--- resources/views/employeeForm.blade.php
+++ resources/views/employeeForm.blade.php
@@ -1,7 +1,7 @@
 <form action="employeeForm" method="post">
     @csrf
     @if($errors->any())
-        {!! implode('', $errors->all('<div style="#e74c3c">:message</div>')) !!}
+        {!! implode('', $errors->all('<div style="color:red">:message</div>')) !!}
     @endif
     <table>
         <tr>
@@ -18,7 +18,7 @@
             <td><input type="text" name="employee[{{$employee->id}}][lastname]" value="{{$employee->last_name}}" /></td>
             <td><input type="text" name="employee[{{$employee->id}}][email]" value="{{$employee->email}}" /></td>
             <td><input type="text" name="employee[{{$employee->id}}][job_role]" value="{{$employee->job_role}}" /></td>
-            <td><input type="checkbox" name="employeedelete" value="1" id="{{$i}}" /></td>
+            <td><input type="checkbox" name="employeedelete" value="1" id="{{$employee->id}}" /></td>
         </tr>
         @endforeach
 
@@ -26,10 +26,37 @@
             <td><input type="text" name="firstname" placeholder="Add new..." /></td>
             <td><input type="text" name="lastname" placeholder="Add new..." /></td>
             <td><input type="text" name="email" placeholder="Add new..." /></td>
-            <td><input type="text" name="jobrole" placeholder="Add new..." /></td>
+            <td><input type="text" name="job_role" placeholder="Add new..." /></td>
         </tr>
     </table>
     {{ $employees->links() }}
     @endif
     <input type="submit" value="Submit!" />
 </form>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
+    $(document).ready( function () {
+
+
+    $(document).on("click",'input[type=checkbox]',function(e){
+        if (confirm('Are you sure you want to delete this ?')) {
+            var id = this.id;
+            $.ajax({
+                url: 'employee/delete/'+id,
+                type: 'post',
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                },
+                success: function(response){
+                    location.reload();
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        } else {
+            return false;
+        }
+    });
+    });
+</script>
